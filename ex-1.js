70ab8e8fff7ce9efb25cb0ee77009fce8ebe2c86
Gravizz <peeke0rr@gmail.com>
2023-12-15 12:51:43 +0700

    feat(ex-1):complete exercise

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,34 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notification, createdTime, content, receiver) {
+    this.notification = notification;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notification, createdTime, content, phoneNumber) {
+    this.notification = notification;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+
+let alertEmail = new EmailNotification(
+  `Alert`,
+  `12.42`,
+  `Content`,
+  `Peeke0rr@gmail.com`
+);
+let alertPhone = new SMSNotification(`Alert`, `12.44`, `Content`, `0912345678`);
+
+alertEmail.send();
+alertPhone.send();
