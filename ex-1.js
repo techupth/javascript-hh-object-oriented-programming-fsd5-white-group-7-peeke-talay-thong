cf69c9c157b09c37d092cd6b2a8d45f9a739d117
Pacharatnkp <pachara.pplzz@gmail.com>
2023-12-21 10:38:10 +0700

    feat:exercise submit

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,28 @@
-class EmailNotification { }
+class EmailNotification {
+    constructor(NotificationID, creatTime, content, receiver) {
+        this.NotificationID = NotificationID;
+        this.creatTime = creatTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`);
+    }
+ }
 
-class SMSNotification { }
+class SMSNotification {
+    constructor(NotificationID, creatTime, content, phoneNumber) {
+        this.NotificationID =NotificationID;
+        this.creatTime = creatTime;
+        this.content = content;
+        this.phoneNumber = phoneNumber;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.phoneNumber}`);
+    }
+}
+const emailNotification = new EmailNotification('001', '2023-01-01', 'New email notification', 'hahah@hotmail.com');
+emailNotification.send();
+
+const smsNotification = new SMSNotification('002', '2023-01-02', 'New SMS notification', '0991234354');
+smsNotification.send();
